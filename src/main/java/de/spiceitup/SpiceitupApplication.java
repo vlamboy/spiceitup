ae6f39df6b9cbcb4d35b55ee151b52ca8b649304
vlamboy <vivienne.lamboy@student.htw-berlin.de>
2022-12-08 17:54:53 +0100

    initial commit

---
+++ src/main/java/de/spiceitup/SpiceitupApplication.java
@@ -0,0 +1,13 @@
+package de.spiceitup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiceitupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiceitupApplication.class, args);
+	}
+
+}
