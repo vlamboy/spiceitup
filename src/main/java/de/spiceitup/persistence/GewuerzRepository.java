f7fb21b788cc16eab6b358736d0841bf1dc6fcf2
vlamboy <vivienne.lamboy@student.htw-berlin.de>
2022-12-14 17:29:16 +0100

    Add Interface GewuerzRepository extending JpaRepository

---
+++ src/main/java/de/spiceitup/persistence/GewuerzRepository.java
@@ -0,0 +1,6 @@
+package de.spiceitup.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GewuerzRepository extends JpaRepository<GewuerzEntity, Long> {
+}
